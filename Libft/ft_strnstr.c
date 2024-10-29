1aef8bf99977dbbe7cdaf3a0aaf123e8b6a82522
Abdellatif Nasser-Allah <nasserallahabdellatif2@gmail.com>
2024-10-30 00:38:56 +0100

    updated

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,24 +6,25 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 22:42:03 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/10/29 12:01:02 by abdnasse         ###   ########.fr       */
+/*   Updated: 2024/10/30 00:37:37 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	const char	*s;
 
-	if (!*little || !len)
+	if (!*little)
 		return ((char *)big);
-	while (*big++ && len--)
+	while (*big && len--)
 	{
 		s = big;
 		while (*s++ == *little++)
 		if(!*little)
-			return ((char *)s);
+			return ((char *)big);
+		big++;
 	}
-
 	return (0);
 }
