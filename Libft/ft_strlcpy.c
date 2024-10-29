bd595d13608b1ab1f9235844d4842518a72c7eb5
Abdellatif Nasser-Allah <nasserallahabdellatif2@gmail.com>
2024-10-29 22:47:16 +0100

    updated

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:07:55 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/10/27 17:57:50 by abdnasse         ###   ########.fr       */
+/*   Updated: 2024/10/29 22:41:42 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,8 +17,11 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	i;
 
 	slen = ft_strlen(src);
+	if (size == 0)
+		return (slen);
 	i = 0;
-	while (*src && i < size - 1)
+	size -= 1;
+	while (*src && i < size)
 		dst[i++] = *src++;
 	dst[i] = '\0';
 	return (slen);
