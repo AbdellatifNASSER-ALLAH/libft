03eaca0f1e34363441746efbcaf3f5bcad0f2be1
Abdellatif Nasser-Allah <nasserallahabdellatif2@gmail.com>
2024-10-29 23:27:49 +0100

    strdup

--- Libft/ft_strdup.c
+++ Libft/ft_strdup.c
@@ -6,12 +6,22 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 23:40:23 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/10/29 11:58:21 by abdnasse         ###   ########.fr       */
+/*   Updated: 2024/10/29 23:27:15 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strdup(const char *s)
 {
-	return ((char *)s);
+	char	*p;
+	size_t	size;
+	
+	size = ft_strlen(s);
+	p = (char *)malloc((size + 1) * sizeof(char));
+	if (!p)
+		return (0);
+	*(p +  size) = 0;
+	while (size--)
+		*(p + size) = *(s + size);
+	return (p);
 }
