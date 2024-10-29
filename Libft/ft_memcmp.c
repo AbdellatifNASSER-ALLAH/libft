593ec4d910e015228cb44d70bccc0314243639b6
Abdellatif Nasser-Allah <nasserallahabdellatif2@gmail.com>
2024-10-29 21:17:27 +0100

    memcmp done

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 22:28:56 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/10/29 11:56:52 by abdnasse         ###   ########.fr       */
+/*   Updated: 2024/10/29 21:16:26 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -18,10 +18,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	p1 = (unsigned char *)s1;
 	p2 = (unsigned char *)s2;
-	while (*p1++ && *p2++ && n--)
+	while (n--)
 	{
 		if (*p1 != *p2)
 			return (*p1 - *p2);
+		p1++;
+		p2++;
 	}
 	return (0);
 }
