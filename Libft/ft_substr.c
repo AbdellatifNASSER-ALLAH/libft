5ebf1d83865be30859c45dfc397217f64d1a944f
Abdellatif Nasser-Allah <nasserallahabdellatif2@gmail.com>
2024-10-30 19:35:29 +0100

    substr

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 16:24:20 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/10/30 17:03:45 by abdnasse         ###   ########.fr       */
+/*   Updated: 2024/10/30 19:32:34 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,8 +15,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*p;
 	size_t	i;
+	size_t	slen;
 
-	p = (char *)malloc((ft_strlen(s) - start + 1) * sizeof(char));
+	slen = ft_strlen(s);
+	if (slen < start)
+		return (ft_strdup("\0"));
+	if (slen - start < len)
+		len = slen - start;
+	p = (char *)malloc((len + 1) * sizeof(char));
 	if (!p)
 		return (0);
 	i = 0;
