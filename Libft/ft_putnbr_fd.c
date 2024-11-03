81ff06937d8b4c52f361c1d63598ab3fc4b53e5f
Abdellatif Nasser-Allah <abdnasse@c3r7p2.1337.ma>
2024-11-03 23:18:43 +0100

    last checks

--- Libft/ft_putnbr_fd.c
+++ Libft/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: abdnasse <abdnasse@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 19:52:29 by abdnasse          #+#    #+#             */
-/*   Updated: 2024/11/03 21:09:52 by abdnasse         ###   ########.fr       */
+/*   Updated: 2024/11/03 23:02:36 by abdnasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,17 +15,23 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (fd < 0)
 		return ;
-	if (n == 0)
-		write(fd, "0", 1);
-	else if (n == -2147483648)
-		write(fd, "-2147483648", 11);
-	else if (n < 0)
+	if (n == -2147483648)
 	{
-		write(fd, "-", 1);
-		n *= -1;
+		ft_putstr_fd("-2147483648", fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-',fd);
+		n = -n;
 	}
 	if (n < 10)
+	{
 		ft_putchar_fd(n + 48, fd);
-	ft_putnbr_fd(fd, n / 10);
-	ft_putchar_fd(n % 10 + 48, fd);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd((n % 10) + 48, fd);
+	}
 }
--- TESTS/main_putnbr_fd.c
+++ TESTS/main_putnbr_fd.c
@@ -4,13 +4,17 @@
 
 int main()
 {
-	int	nbrs[] = {0, 1, 111, INT_MIN, INT_MAX};
+	int	nbrs[] = {0, 1, 123, INT_MIN, INT_MAX};
 	size_t	i = 0;
 	int	fd = 1;
-
+	
+	while (i < sizeof(nbrs) / sizeof(int))
+		printf("std|%d\n", nbrs[i++]);
+	i = 0;
 	while (i < sizeof(nbrs) / sizeof(int))
 	{
-		ft_putnbr_fd(nbrs[i], fd);
-		write(fd, "\n", 1);
+		ft_putstr_fd("ft_|", fd);
+		ft_putnbr_fd(nbrs[i++], fd);
+		ft_putstr_fd("\n", fd);
 	}
 }
